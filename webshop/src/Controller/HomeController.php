6e36d561ad2b056c8370c1cd871d69d6dd628d3e
IvanDrago <ivandragosoldo@gmail.com>
2020-10-26 12:53:14 +0100

    Product listing implemented

--- webshop/src/Controller/HomeController.php
+++ webshop/src/Controller/HomeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
+     * @param ProductRepository $productRepository
+     * @return Response
      */
-    public function index()
+    public function index(ProductRepository $productRepository)
     {
+        $products = $productRepository->findAll();
+
         return $this->render('home/index.html.twig', [
+            'products' => $products
         ]);
     }
 }
