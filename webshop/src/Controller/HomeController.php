3e572b329061d13d24897dbc54ef654083845b66
IvanDrago <ivandragosoldo@gmail.com>
2020-10-27 16:28:51 +0100

    Categories displayed dynamically. Products displayed by categories.

--- webshop/src/Controller/HomeController.php
+++ webshop/src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,14 +13,17 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      * @param ProductRepository $productRepository
+     * @param CategoryRepository $categoryRepository
      * @return Response
      */
-    public function index(ProductRepository $productRepository)
+    public function index(ProductRepository $productRepository, CategoryRepository $categoryRepository)
     {
         $products = $productRepository->findAll();
+        $categories = $categoryRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'products' => $products,
+            'categories' => $categories,
         ]);
     }
 
