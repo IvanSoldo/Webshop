1102a633a2f9d2379dd5e98e8a6a782e86e28450
IvanDrago <ivandragosoldo@gmail.com>
2020-10-28 23:55:20 +0100

    Orders to pdf impl

--- webshop/src/Controller/Admin/OrderCrudController.php
+++ webshop/src/Controller/Admin/OrderCrudController.php
@@ -5,12 +5,13 @@ namespace App\Controller\Admin;
 use App\Entity\Order;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\NumberField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
-
 
 class OrderCrudController extends AbstractCrudController
 {
@@ -21,18 +22,25 @@ class OrderCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+
         return $actions
-            ->disable(Action::DELETE, Action::NEW);
+            ->disable(Action::DELETE, Action::NEW)
+            ->add(Crud::PAGE_INDEX, Action::DETAIL)
+            ->update(Crud::PAGE_INDEX, Action::DETAIL, function (Action $action) {
+                return $action->setLabel('Details');
+            });
     }
 
     public function configureFields(string $pageName): iterable
     {
 
         return [
-            TextField::new('user')->onlyOnIndex(),
-            DateTimeField::new('order_date')->onlyOnIndex(),
-            ArrayField::new('orderProducts', 'Products')->onlyOnIndex(),
-            NumberField::new('totalSum', 'Subtotal')->onlyOnIndex()->formatValue(function ($value){
+            TextField::new('user.getEmail', 'Email')->hideOnForm(),
+            TextField::new('user', 'Name')->onlyOnDetail(),
+            TextField::new('getAddress', 'Address')->hideOnForm(),
+            DateTimeField::new('order_date')->hideOnForm(),
+            ArrayField::new('orderProducts', 'Products')->hideOnForm()->setSortable(false),
+            NumberField::new('totalSum', 'Subtotal')->hideOnForm()->formatValue(function ($value){
               return $value = '$' . strval($value);
             })
         ];
