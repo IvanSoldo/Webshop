4ebe34d5d524ba1d253750c9834d708ddced01b1
IvanDrago <ivandragosoldo@gmail.com>
2020-10-29 13:11:19 +0100

    Display bug fixed

--- webshop/src/Controller/Admin/ProductCrudController.php
+++ webshop/src/Controller/Admin/ProductCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
-use EasyCorp\Bundle\EasyAdminBundle\Collection\FieldCollection;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -11,10 +10,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ArrayField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\CollectionField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\Field;
-use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProductCrudController extends AbstractCrudController
@@ -43,8 +39,8 @@ class ProductCrudController extends AbstractCrudController
             BooleanField::new('product_active'),
             BooleanField::new('onDiscount'),
             Field::new('discountPercentage'),
-            ImageField::new('pictureFile')->setFormType(VichImageType::class)->hideOnIndex(),
-            ImageField::new('picture')->setBasePath('/images/products')->onlyOnIndex()
+            Field::new('pictureFile')->setFormType(VichImageType::class)->onlyWhenCreating(),
+
         ];
 
         if ($pageName === Crud::PAGE_INDEX) {
